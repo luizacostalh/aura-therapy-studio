1929b852555e4ea29008713bda572ddb73a67ea8
Luiza Costa <luizacostalh@gmail.com>
2026-01-11 18:09:22 -0300

    atendimento

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       <div className="container-custom py-4">
         <div className="flex items-center justify-between">
           <a href="#" className="font-display text-xl md:text-2xl font-semibold text-green-dark">
-            
+            Atendimento de segunda a sexta.
           </a>
 
           {/* Desktop Navigation */}
